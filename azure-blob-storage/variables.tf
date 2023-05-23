228211d4e7ef468c9580e3140bb7db6c0dd34cd5
Jenuarius <101048027+Jenuarius@users.noreply.github.com>
2023-05-23 18:36:44 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,23 +1,19 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "example-resources"
 }
 
 variable "storage_account_name" {
   description = "Name of the storage account"
   type        = string
-  default     = "examplestoracc"
 }
 
 variable "storage_container_name" {
   description = "Name of the storage container"
   type        = string
-  default     = "content"
 }
 
 variable "storage_blob_name" {
   description = "Name of the storage blob"
   type        = string
-  default     = "my-awesome-content.zip"
 }
