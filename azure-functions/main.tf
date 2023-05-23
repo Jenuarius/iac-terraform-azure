5cd5b22eb7ef9d8f598d665f6c411cb74284e9ca
Jenuarius <101048027+Jenuarius@users.noreply.github.com>
2023-05-23 16:40:36 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -35,6 +35,18 @@ resource "azurerm_function_app" "example" {
   version                    = "~3"
 }
 
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
+
 resource "azurerm_function_app_function" "example" {
   name            = "example-function-app-function"
   function_app_id = azurerm_linux_function_app.example.id
